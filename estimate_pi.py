1c847723668eddc35fb325fc4953db26ef64ada9
Vartika Sood <sood.vartika9905@gmail.com>
2025-12-16 23:57:37 +0100

    'Done!'

--- estimate_pi.py
+++ estimate_pi.py
@@ -1,7 +1,19 @@
+import numpy as np
+
 def estimate_pi(num_samples):
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    darts = 0
+
+    for i in range(num_samples):
+        x = np.random.rand()
+        y = np.random.rand()
+        distance = (x*x + y*y)** 0.5
+
+        if distance < 1:
+            darts += 1
+
+    pi_estimate = 4 * (darts / num_samples)
+
+    return pi_estimate
+
+
+print(estimate_pi(1000))
--- get_random_subset_of_naturals_up_to_20.py
+++ get_random_subset_of_naturals_up_to_20.py
@@ -1,9 +1,8 @@
+import numpy as np
+
 def get_random_subset_of_naturals_up_to_20():
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    mask = np.random.randint(0, 2, size = 20)
+    n = np.arange(1, 21)
+    sub = n[mask == 1]
 
-
+    return sub
